007383f6c56dca136afb29a08daf934ea4e3c8a6
Tahsin <zadidtausif@gmail.com>
2025-02-13 02:20:17 +0600

    initial commit

--- demo.java
+++ demo.java
@@ -3,6 +3,8 @@ public class demo {
         int nums [] = {5,7,9,11,13};
         int target = 11;
         int result = LinearSearch(nums , target);
+        int result1 = BinarySearch(nums , target);
+
             if(result != -1){
                 System.out.println("Element found at index :" + result);
             }
@@ -19,5 +21,26 @@ public class demo {
                     }
                 }
                 return -1;
+            }
+
+            public static int BinarySearch(int [] nums, int target){
+                int left = 0;
+                int right = nums.length -1;
+
+                while(left <= right){
+                    int mid = (left + right)/2;
+
+                    if(nums[mid] == target){
+                        return mid;
+                    }
+                    else if(nums[mid] < target){
+                        left = mid +1;
+                    }
+                    else{
+                        right = mid - 1;
+                    }
+                }
+                return -1;
+
             }
 }
