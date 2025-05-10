0fed334ab36d2dc380af649f09ba588737ec95f2
Tahsin <155565967+tahsin201399@users.noreply.github.com>
2025-05-10 10:51:26 +0600

    Add files via upload

---
+++ Lab-04/HashTable/Task1/PairNode.java
@@ -0,0 +1,13 @@
+public class PairNode {
+    
+    String key;
+    Integer value;
+    PairNode next;
+
+    public PairNode( String k, Integer v ){
+        this.key = k;
+        this.value = v;
+        this.next = null;
+    }
+    
+}
