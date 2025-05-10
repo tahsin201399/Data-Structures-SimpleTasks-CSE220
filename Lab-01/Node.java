19b6506b0429ed29559c9779e2b8fbba67934a8e
Tahsin <155565967+tahsin201399@users.noreply.github.com>
2025-05-10 10:44:06 +0600

    Add files via upload

---
+++ Lab-01/Node.java
@@ -0,0 +1,17 @@
+class Node{
+
+    public final String value = null;
+    Object elem;
+    Node next;
+
+    public Node(Object elem, Node next){
+        this.elem = elem;
+        this.next = next;
+    }
+    
+    //Overloading
+    public Node(Object elem){
+        this.elem = elem;
+        this.next = null;
+    }
+}
---
+++ Lab-01/Task4.java
@@ -0,0 +1,36 @@
+// Task03: Assemble Conga Line
+public class Task4 {
+
+    public static Boolean assembleCongaLine(Node head){
+        
+        Node x = head;
+        while (x.next != null){
+            if((int)x.elem > (int)x.next.elem){
+                return false;
+            }
+            x=x.next;
+        }
+
+        return true;
+    }
+
+    //NOTE: if you find any issue with the driver code please inform AIB
+    //DO NOT TOUCH THE DRIVER CODE BELOW
+    public static void main(String[] args){
+
+        System.out.println("==============Test Case 1=============");
+        Node congaLine = LinkedList.createList(new Integer[]{10,15,34,41,56,72});
+        System.out.print("Original Conga Line: ");
+        LinkedList.printLL(congaLine);
+        System.out.println("Expected output: true");
+        System.out.println( "Your output: "+assembleCongaLine(congaLine) ); //This should print True
+        System.out.println();
+        System.out.println("==============Test Case 2=============");
+        congaLine = LinkedList.createList(new Integer[]{10,15,44,41,56,72});
+        System.out.print("Original Conga Line: ");
+        LinkedList.printLL(congaLine);
+        System.out.println("Expected output: false");
+        System.out.println( "Your output: "+assembleCongaLine(congaLine) ); //This should print False
+        System.out.println();
+    }
+}
